55a2a1dd7ab480b0672cc0f05f5ca89e2311aeba
Do Jaewoo <herobine159@naver.com>
2023-08-06 20:36:32 +0900

    여덟 번째 커밋
    경로 매개변수 Path 클래스 사용 및 쿼리 매개변수 주석 추가

--- todo.py
+++ todo.py
@@ -1,6 +1,6 @@
 
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
 from model import Todo
 
 todo_router=APIRouter()             # APIRouter() 인스턴스 생성
@@ -24,9 +24,14 @@ async def retrieve_todos() -> dict:    # 파라미터 없이 호출, 반환 타�
 # 경로 매개변수 : 리소스를 식별하기 위해 API 라우팅에서 사용 -> 식별자 역할을 하며 웹 애플리케이션이 추가 처리를 할 수 있도록 연결 고리가 되기도 함.
 # 앞서 할 일 (todo)를 추가하거나 모든 할 일 목록 (todo_list)를 추출하는 라우트를 만들었다.
 # 아래서는 하나의 todo작업만 추출하는 새로운 라우트를 만든다.
+# Path 클래스 추가 ->  FastAPI가 제공하는 클래스로, 라우트 함수에 있는 다른 인수와 경로 매개변수를 구분하는 역할을 함.
+# Path 클래스는 스웨거와 ReDoc 등으로 OpenAPI 기반 문서를 자동 생성할 때 라우트 관련 정보를 함께 문서화하도록 도움.
+# 첫 인수로 None 또는 ...을 받을 수 있다. 
+# 첫 번째 인수가 ...이면 경로 매개변수를 반드시 지정해야 함. 또한 경로 매개변수가 숫자이면 수치 검증을 위한 인수를 지정할 수 있다.
+# 예를 들어 gt(greater than), le와 같은 검증 기호를 사용할 수 있다. 이를 통해 경로 매개변수에 사용된 값이 특정 범위에 있는 숫자인지 검증이 가능.
 
 @todo_router.get("/todo/{todo_id}")     # {todo_id} : 경로 매개변수 -> 애플리케이션이 지정한 ID와 일치하는 todo 작업을 반환할 수 있다.
-async def get_single_todo(todo_id: int) -> dict:
+async def get_single_todo(todo_id: int=Path(..., title="The ID of the todo to retrieve.")):     
     for todo in todo_list:
         if todo.id == todo_id:
             return{
@@ -35,6 +40,14 @@ async def get_single_todo(todo_id: int) -> dict:
     return {
         "message" : "Todo with supplied ID doesn't exist."
     }
+
+# 쿼리 매개변수 : 선택 사항이며 보통 URL에서 ? 뒤에 나옴.
+# 제공된 쿼리를 기반으로 특정한 값을 반환하거나 요청을 필터링할 때 사용된다.
+# 쿼리는 라우트 처리기의 인수로 사용되지만 경로 매개변수와 다른 형태로 정의된다.
+# 예를 들어 아래와 같이 FastAPI Query 클래스의 인스턴스를 만들어서 라우트 처리기의 인수로 쿼리를 정의할 수 있음.
+
+# async query_route(query:str=Query(None)):
+#   return query
 
 ## curl 실행 시 Not found 
 ## APIRouter 클래스는 FastAPI 클래스와 동일한 방식으로 작동
